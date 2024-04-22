9c326c5fcd56a911d6ee4fa1bf12b89b764e8172
Fidan Hykolli <fidan-323@live.com>
2024-04-22 16:07:23 +0200

    Class Utent
    Service, controller, repository, entities

---
+++ src/main/java/repositories/UtenteRepository.java
@@ -0,0 +1,10 @@
+package repositories;
+
+import entities.Utente;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+}
