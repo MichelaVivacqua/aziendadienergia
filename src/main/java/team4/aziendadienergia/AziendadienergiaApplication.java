90be76b52c969ef793e7948841f3320d3e8e4a85
MichelaVivacqua <vivacquamichela@gmail.com>
2024-04-22 11:35:44 +0200

    Initial commit

---
+++ src/main/java/team4/aziendadienergia/AziendadienergiaApplication.java
@@ -0,0 +1,13 @@
+package team4.aziendadienergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AziendadienergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AziendadienergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/team4/aziendadienergia/AziendadienergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package team4.aziendadienergia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AziendadienergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
