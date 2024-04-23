71c67ad8c137f5787e240c2893d03ba66a4c04b6
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-23 12:04:33 +0200

    AdressDatabaseLoader updated to do not inject duplicated scores

--- src/main/java/team4/aziendadienergia/utility/AdressDatabaseLoader.java
+++ src/main/java/team4/aziendadienergia/utility/AdressDatabaseLoader.java
@@ -10,7 +10,10 @@ import team4.aziendadienergia.repositories.ProvinceDAO;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Component
 public class AdressDatabaseLoader {
@@ -25,27 +28,44 @@ public class AdressDatabaseLoader {
     ProvinceDAO provinceDAO;
 
     @PostConstruct
-    public void iniettaAdress() throws Exception {
+    public void iniettaAddress() throws Exception {
+        List<Province> existingProvinces = provinceDAO.findAll();
+        List<Municipality> existingMunicipalities = municipalityDAO.findAll();
+
         Path csvPath1 = Paths.get("src/main/java/team4/aziendadienergia/CSV/comuni-italiani.csv");
         Path csvPath2 = Paths.get("src/main/java/team4/aziendadienergia/CSV/province-italiane.csv");
 
         List<String[]> csvProvincia = myCSVReader.readCsv(csvPath2);
-        csvProvincia.forEach(rigaCSVProvincia -> {
-            Province province = new Province();
-            province.setCodiceProvincia(rigaCSVProvincia[0]);
-            province.setName(rigaCSVProvincia[1]);
-            province.setCountry(rigaCSVProvincia[2]);
-            provinceDAO.save(province);
-        });
-
         List<String[]> csvMunicipality = myCSVReader.readCsv(csvPath1);
-        csvMunicipality.forEach(rigaCSVMunicipality -> {
-            Municipality municipality = new Municipality();
-            municipality.setPostalCode(String.valueOf(rigaCSVMunicipality[0] ) + String.valueOf(rigaCSVMunicipality[1]));;
-            municipality.setName(rigaCSVMunicipality[2]);
-            municipality.setProvinceCity(rigaCSVMunicipality[3]);
-            municipalityDAO.save(municipality);
-        });
 
+        List<Province> newProvinces = csvProvincia.stream()
+                .map(row -> {
+                    Province province = new Province();
+                    province.setCodiceProvincia(row[0]);
+                    province.setName(row[1]);
+                    province.setCountry(row[2]);
+                    return province;
+                })
+                .collect(Collectors.toList());
+
+        List<Municipality> newMunicipalities = csvMunicipality.stream()
+                .map(row -> {
+                    Municipality municipality = new Municipality();
+                    municipality.setPostalCode(String.valueOf(row[0]) + String.valueOf(row[1]));
+                    municipality.setName(row[2]);
+                    municipality.setProvinceCity(row[3]);
+                    return municipality;
+                })
+                .collect(Collectors.toList());
+
+        Set<Province> newProvincesSet = new HashSet<>(newProvinces);
+        newProvincesSet.removeAll(existingProvinces);
+
+        Set<Municipality> newMunicipalitiesSet = new HashSet<>(newMunicipalities);
+        newMunicipalitiesSet.removeAll(existingMunicipalities);
+
+        newProvincesSet.forEach(provinceDAO::save);
+        newMunicipalitiesSet.forEach(municipalityDAO::save);
     }
+
 }
