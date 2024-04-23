ac8d5e0cea820a017e3b9f33af8168886e1cafba
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-04-23 12:14:32 +0200

    Update CSV Reader

--- src/main/java/team4/aziendadienergia/repositories/ProvinceDAO.java
+++
@@ -1,9 +0,0 @@
-package team4.aziendadienergia.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import team4.aziendadienergia.entities.Province;
-
-import java.util.UUID;
-
-public interface ProvinceDAO extends JpaRepository<Province, UUID> {
-}
