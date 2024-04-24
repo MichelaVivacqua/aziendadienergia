cf40eba54e4c5c0d353d1c077a91fcc2b0e0e4f3
Nick-DiGiacinto <151628711+Nick-DiGiacinto@users.noreply.github.com>
2024-04-24 16:32:53 +0200

    small fix to repositories

--- src/main/java/team4/aziendadienergia/repositories/MunicipalityDAO.java
+++ src/main/java/team4/aziendadienergia/repositories/MunicipalityDAO.java
@@ -1,9 +1,11 @@
 package team4.aziendadienergia.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import team4.aziendadienergia.entities.Municipality;
 
 import java.util.UUID;
 
+@Repository
 public interface MunicipalityDAO extends JpaRepository<Municipality, UUID> {
 }
